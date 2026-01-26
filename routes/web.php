448b5521367675f788016cb8851bc9789e02f377
Vinsara Senanayake <vinsarasenanayake@gmail.com>
2026-01-26 19:00:59 +0530

    Cleaned the codes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\ProductController;
 
 // Public Routes
 Route::middleware(['redirectAdmin'])->group(function () {
